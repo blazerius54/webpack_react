237117c0b78715737bc3cf20ca34e1523ed7e89e
VladimirKasharevsky <kashar015@ambler.ru>
2023-03-12 19:47:08 +0300

    add loader

--- src/pages/EmployeeCard.tsx
+++ src/pages/EmployeeCard.tsx
@@ -4,6 +4,7 @@ import {Link, useParams} from 'react-router-dom'
 import checkWebsite from "../Utils/AppUtil";
 import ButtonCustom from "../components/ButtonCustom";
 import EmployeeEdit from "./EmployeeEdit";
+import Loader from "../components/Loader";
 
 interface Employee {
     name: string,
@@ -18,54 +19,58 @@ export default function EmployeeCard() {
     const {id} = useParams();
     const [data, setData] = useState<Employee | null>(null);
     const [isEdit, setEdit] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false)
 
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users/' + id)
             .then(response => response.json())
             .then(json => setData(json))
+        setIsLoaded(true);
     }, [id])
 
     const onclick = () => {
         setEdit(true)
     }
     return (
-        <>
-            {isEdit.valueOf() ? <EmployeeEdit
-                    email={data.email}
-                    name={data.name}
-                    phone={data.phone}
-                    username={data.username}
-                    website={data.website}/>
-                :
-                data && (
-                    <>
-                        <div>
-                            <p>
-                                name: {data.name}
-                            </p>
-                            <p>
-                                email: {data.email}
-                            </p>
-                            <p>
-                                phone: {data.phone}
-                            </p>
-                            <p>
-                                username: {data.username}
-                            </p>
-                            {checkWebsite(data.website).valueOf() ?
+        isLoaded ?
+            <>
+                {isEdit ? <EmployeeEdit
+                        email={data.email}
+                        name={data.name}
+                        phone={data.phone}
+                        username={data.username}
+                        website={data.website}/>
+                    :
+                    data && (
+                        <>
+                            <div>
                                 <p>
-                                    website: {data.website}
+                                    name: {data.name}
                                 </p>
-                                : null}
-                        </div>
-                        <div>
-                            <ButtonCustom onClick={onclick} isDisabled={isEdit} buttonName={"Edit"}/>
-                            <ButtonCustom onClick={null} isDisabled={!isEdit} buttonName={"Save"}/>
-                        </div>
-                    </>
-                )
-            }
-        </>
+                                <p>
+                                    email: {data.email}
+                                </p>
+                                <p>
+                                    phone: {data.phone}
+                                </p>
+                                <p>
+                                    username: {data.username}
+                                </p>
+                                {checkWebsite(data.website).valueOf() ?
+                                    <p>
+                                        website: {data.website}
+                                    </p>
+                                    : null}
+                            </div>
+                            <div>
+                                <ButtonCustom onClick={onclick} isDisabled={isEdit} buttonName={"Edit"}/>
+                                <ButtonCustom onClick={null} isDisabled={!isEdit} buttonName={"Save"}/>
+                            </div>
+                        </>
+                    )
+                }
+            </> :
+            <Loader/>
     )
 }
--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,27 +1,33 @@
 import React, {useEffect, useState} from 'react'
 import '/src/App.css';
 import {Link} from "react-router-dom";
+import Loader from "../components/Loader";
 
 
 export default function EmployeeList() {
 
     const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users/')
             .then(response => response.json())
             .then(json => setData(json))
-        }, [])
+        setIsLoaded(true);
+    }, [])
 
     return (
-        <div>
-            <ul>
-                {data.map(record => (
-                    <Link  key={record.id} to={'/employee/' + record.id}>
-                        <li>{record.name}</li>
-                    </Link>
-                ))}
-            </ul>
-        </div>
-    );
+        isLoaded ?
+            <div>
+                <ul>
+                    {data.map(record => (
+                        <Link key={record.id} to={'/employee/' + record.id}>
+                            <li>{record.name}</li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            :
+            <Loader/>
+        );
 }
